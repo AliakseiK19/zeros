2089e2444150d4d109916f860092853b79c1c3b9
AliakseiK19 <positive_on@yahoo.com>
2018-09-25 20:08:53 +0300

    -)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function getZerosCount(number) {
   // your implementation
-}
+       
+            var numA = 0;
+            var numB = 0;
+            var arrPow = [5, 25, 125, 625, 3125, 15625, 78125, 390625, 1953125, 9765625, 48828125, 244140625];
+            for (let i = 0; arrPow[i] <= number; i++) {
+                if (number % arrPow[i] == 0) {
+                    numB += number / arrPow[i];
+                }
+                else {
+                    numA = number / arrPow[i];
+                    numA = numA - (numA % 1);
+                    numB += numA;
+                }
+            }
+            return numB;
+        }        
+    
